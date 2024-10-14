7f9e43e9b3af0efcfc3e41ce3dcf041a270af65c
namahu <neti0326@gmail.com>
2024-10-14 18:26:42 +0900

    style: 💄 不要なコードをコメントアウト

--- src/App.tsx
+++ src/App.tsx
@@ -1,85 +1,79 @@
-import React, { useEffect, useState } from 'react';
-import { nanoid } from "nanoid";
+import React from 'react';
+// import { nanoid } from "nanoid";
 
 import { Sidebar } from './components/Sidebar/Sidebar';
 import { TaskList } from "@/features/Task/components/TaskList";
-import { TaskProps } from './features/Task/components/Task';
-import Form, { NewTask } from './components/Form'
+// import Form from './components/Form'
 
 import './App.css'
 
-type FetchedTask = Omit<TaskProps["task"], "updateTask">[];
-
 const App: React.FC = () => {
 
-  const [tasks, setTasks] = useState<FetchedTask>([]);
+  // const [tasks, setTasks] = useState<FetchedTask>([]);
 
-  useEffect(() => {
-    const getTasks = async () => {
-      const response = await fetch("http://localhost:3000/tasks");
-      const fetchedTasks = await response.json();
-      setTasks(fetchedTasks);
-    };
-    getTasks();
-  }, []);
+  // useEffect(() => {
+  //   const getTasks = async () => {
+  //     const response = await fetch("http://localhost:3000/tasks");
+  //     const fetchedTasks = await response.json();
+  //     setTasks(fetchedTasks);
+  //   };
+  //   getTasks();
+  // }, []);
 
-  const addTask = async (taskData: NewTask) => {
-    const newTask = {
-      id: `todo-${nanoid()}`,
-      title: taskData.title,
-      startDate: taskData.startDate,
-      dueDate: taskData.dueDate,
-      done: false,
-      deleted: false
-    };
-    const response = await fetch("http://localhost:3000/tasks", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify(newTask)
-    });
-    if (!response.ok) {
-      window.alert("Failed to add task");
-      return;
-    }
-    setTasks([...tasks, newTask]);
-  };
+  // const addTask = async (taskData: NewTask) => {
+  //   const newTask = {
+  //     id: `todo-${nanoid()}`,
+  //     title: taskData.title,
+  //     startDate: taskData.startDate,
+  //     dueDate: taskData.dueDate,
+  //     done: false,
+  //     deleted: false
+  //   };
+  //   const response = await fetch("http://localhost:3000/tasks", {
+  //     method: "POST",
+  //     headers: {
+  //       "Content-Type": "application/json"
+  //     },
+  //     body: JSON.stringify(newTask)
+  //   });
+  //   if (!response.ok) {
+  //     window.alert("Failed to add task");
+  //     return;
+  //   }
+  //   setTasks([...tasks, newTask]);
+  // };
 
-  const updateTask = (
-    id: string,
-    updateProperty: {
-      key: string,
-      value: string | boolean
-    }
-  ) => {
-    const updatedTasks = tasks.map(task => {
-      if (task.id === id) {
-        return { ...task, [updateProperty.key]: updateProperty.value };
-      }
-      return task;
-    });
-    setTasks(updatedTasks);
+  // const updateTask = (
+  //   id: string,
+  //   updateProperty: {
+  //     key: string,
+  //     value: string | boolean
+  //   }
+  // ) => {
+  //   const updatedTasks = tasks.map(task => {
+  //     if (task.id === id) {
+  //       return { ...task, [updateProperty.key]: updateProperty.value };
+  //     }
+  //     return task;
+  //   });
+  //   setTasks(updatedTasks);
 
-    fetch(`http://localhost:3000/tasks/${id}`, {
-      method: "PATCH",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify({ [updateProperty.key]: updateProperty.value })
-    });
-  };
+  //   fetch(`http://localhost:3000/tasks/${id}`, {
+  //     method: "PATCH",
+  //     headers: {
+  //       "Content-Type": "application/json"
+  //     },
+  //     body: JSON.stringify({ [updateProperty.key]: updateProperty.value })
+  //   });
+  // };
 
   return (
     <>
       <Sidebar />
       <main>
         <h1 className="title">Todo App sample</h1>
-        <Form onSubmit={addTask} />
-        <TaskList
-          tasks={tasks}
-          updateTask={updateTask}
-        />
+        {/* <Form onSubmit={addTask} /> */}
+        <TaskList />
       </main>
     </>
   )
