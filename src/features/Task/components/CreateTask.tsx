a63df673dd8840d6720269f97003472875abbb10
namahu <neti0326@gmail.com>
2024-10-20 15:18:45 +0900

    feat: 🎸 タスク作成フォームを追加

--- src/features/Task/components/CreateTask.tsx
+++ src/features/Task/components/CreateTask.tsx
@@ -1,24 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
+
+import { useAllProjects } from "@/features/Project/api/get-projects";
 
 export const CreateTask: React.FC = () => {
+    const { projects, isLoading } = useAllProjects();
+    const [isOpen, setIsOpen] = useState(false);
+
+    if (!isOpen) {
+        return (
+            <button onClick={() => setIsOpen(!isOpen)}>Add Task</button>
+        );
+    }
+
     return (
         <div>
-            <button>Add Task</button>
-            <form>
-                <label>
-                    Title:
-                    <input type="text" name="title" />
-                </label>
-                <label>
-                    Start Date:
-                    <input type="date" name="startDate" />
-                </label>
-                <label>
-                    Due Date:
-                    <input type="date" name="dueDate" />
-                </label>
-                <button type="submit">Add Task</button>
-            </form>
+            <div className="taskCreateForm">
+                <div className="formItem">
+                    <input type="text" name="title" placeholder="Task name" />
+                </div>
+                <div className="formItem">
+                    <input type="text" name="description" placeholder="Description" />
+                </div>
+                <div className="formItem">
+                    <input type="date" name="startDate" placeholder="Task name" />
+                </div>
+                <div className="formItem">
+                    <input type="date" name="dueDate" placeholder="Task name" />
+                </div>
+                <div className="formItem">
+                    <select>
+                        {isLoading
+                            ? <option>loading...</option>
+                            : projects.map((project) => (
+                                <option key={project.id} value={project.id}>
+                                    {project.name}
+                                </option>
+                            ))}
+                    </select>
+                </div>
+                <button type="button" onClick={() => setIsOpen(!isOpen)}>Cancel</button>
+                <button type="button">Add Task</button>
+            </div>
         </div>
     );
 };
