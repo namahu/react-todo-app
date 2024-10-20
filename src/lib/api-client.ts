6996091a39151868ffb0367c2a3d37ea125c55f9
namahu <neti0326@gmail.com>
2024-10-20 16:28:32 +0900

    feat: 🎸 post を追加

--- src/lib/api-client.ts
+++ src/lib/api-client.ts
@@ -7,6 +7,16 @@ export const api = {
         console.log(json);
         return json;
     },
+    post: async <T>(endpoint: string, data: T) => {
+        const response = await fetch(`${baseUrl}${endpoint}`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(data)
+        });
+        return await response.json();
+    },
     updateTask: async (id: string, updateProperty: { key: string, value: string | boolean }) => {
         const response = await fetch(`http://localhost:3001/tasks/${id}`, {
             method: "PATCH",
