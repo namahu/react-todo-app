7f8591516f0a1e1788100217e670a293d2c9f24d
namahu <neti0326@gmail.com>
2024-11-02 17:56:45 +0900

    refactor: 💡 作成したプロバイダーの導入

--- src/App.tsx
+++ src/App.tsx
@@ -6,17 +6,18 @@ import { TaskList } from "@/features/Task/components/TaskList";
 import './App.css'
 
 import { TaskContextProvider } from "@/features/Task/context/task-context";
+import { ProjectContextProvider } from './features/Project/context/project-context';
 
 const App: React.FC = () => {
   return (
-    <>
+    <ProjectContextProvider>
       <Sidebar />
       <main>
         <TaskContextProvider>
           <TaskList />
         </TaskContextProvider>
       </main >
-    </>
+    </ProjectContextProvider>
   )
 }
 
