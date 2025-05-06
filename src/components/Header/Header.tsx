772ef1d43ebce6f6128ca13f14fd487e1b050883
namahu <neti0326@gmail.com>
2025-05-06 13:36:16 +0900

    feat: 🎸 ログアウト処理を追加

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,11 +2,23 @@ import React from 'react';
 
 import styles from "./styles/header.module.css";
 import { CreateTask } from '@/features/Task/components/CreateTask';
+import { Button } from '../ui/button/button';
+import { signOut } from 'firebase/auth';
+import { useFirebase } from '@/lib/firebase/context/firebsae-context';
+import { useNavigate } from 'react-router';
 
 export const Header: React.FC = () => {
+    const { auth } = useFirebase();
+    const navigate = useNavigate();
+    const handleLogout = async () => {
+        await signOut(auth);
+        navigate("/login");
+    };
+
     return (
         <header className={styles.header}>
             <div className={styles["header-menu"]}>
+                <Button onClick={handleLogout}>ログアウト</Button>
                 <CreateTask />
             </div>
         </header>
